a192554da0a2667d9d8a0e6d7e8cdb09fd363cb1
emmchier <emmchierchie@gmail.com>
2021-05-12 21:29:16 -0300

    exploring api options

--- src/pages/DashboardScreen.js
+++ src/pages/DashboardScreen.js
@@ -1,71 +1,82 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import Imgix, { Background } from 'react-imgix';
+import Imgix from 'react-imgix';
+
+import { useFetchImgs } from '../hooks/useFetchImgs';
 
 export const DashboardScreen = () => {
 
-    const images = [
-        "forest1",
-        "forest2",
-        "forest3",
-        "mountain1",
-        "mountain2",
-        "mountain3",
-        "river1",
-        "river2",
-        "river3"
-      ];
+      const { data, loading } = useFetchImgs();
       
-      const buildURL = imagePath =>
-        `https://assets.imgix.net/tutorials/${imagePath}.webp`;
+    //   const buildURL = imagePath =>
+    //     `https://assets.imgix.net/tutorials/${imagePath}.webp`;
+
+    const [myWidth, setMyWidth] = useState(200);
+
+    const handleClick = (number) => {
+        setMyWidth(number);
+    }
       
     return (
-        <>
-        <Background
-            src="https://assets.imgix.net/tutorials/forest4.webp"
-            className="hero-image"
-            //imgixParams={{ w: 1920, h: 500 }}
 
-        >
-            <h2>Responsive Image Gallery with React and imgix</h2>
-        </Background>
-    
+        <>
+        <div className="container alignX mb-30">
+            <button className="btn btn-primary" onClick={ ()=> handleClick(600) }>width: 600</button>
+            <button className="btn btn-primary" onClick={ ()=> handleClick(1200) }>width: 1200</button>
+            <button className="btn btn-primary" onClick={ ()=> handleClick(1800) }>width: 1800</button>
+        </div>
+       
          <div className="container">
 
-           
-
-            {images.map(image => (
-            <Imgix
-                sizes="(min-width: 960px) 33vw, (min-width: 640px) 50vw, 100vw"
-                src={buildURL(image)}
-                key={image}
-                imgixParams={{
-                fit: "crop",
-                fm: "jpg"
-                }}
-                width="600"
-                height="600"
-            />
-            ))}
-
-            {/*<div className="row">
- 
                 {   
                     loading
                     ? 'Loading...'
-                    : data.map( (image, key) => {
+                    : data.map( image => {
                         return (
-                            <ImageItem 
-                                key={ key }
-                                { ...image }
+                            <Imgix
+                                sizes="(min-width: 960px) 33vw, (min-width: 640px) 50vw, 100vw"
+                                src={image.url}
+                                key={image}
+                                imgixParams={{
+                                    // adjustment (all sliders -100 to 100. 0 default)
+                                    con: 0, // constrast
+                                    bri: 0, // brigtness
+                                    exp: 0, // exposure= light
+                                    gam: 0, // gamma brigtness in same color
+                                    high: 100, // opacitiy (-100 to 0)
+                                    hue: 0, // filter color (0 to 359)
+                                    invert: false, // negative effect (default false - switch component)
+                                    sat: 0, // saturation 
+                                    shad: 0, // shadow (0 to 100)
+                                    sharp: 0, // (0 to 100) (NO USE)
+                                    usm: 25, // use with usmrad (NO USE ALONE)
+                                    usmrad: 0, // usm:25 + usmrad: 0 to 500 - mask effect
+                                    vib: 0, // intensity
+                                    
+                                    // rotation
+                                    flip: '', // invert horiz or vert (select component) none='' , horizontal='h', vertical='v', diagonal='hv' or 'vh
+                                    orient: 0, // rotation in 4 directions (4 icon btn rotate) default=0, right=90, 180, 270
+                                    rot: 0, // rotation exact effect (slider) 0 to 359
+
+                                    // borders
+                                    pad: '', // 0 to 500. default = ''(slider)
+                                    'pad-left': '', // 0 to 500. default = ''. (select component)
+                                    
+                                    // shapes
+                                    mask: 'corners', // forms (ellipse, width & height most be same) | corners
+                                  
+                                    // background
+                                    bg: '#00', // background color. default transparent= #00 (select color component)
+
+                                }}
+                                width={ myWidth }
+                                height={ 600 }
                             />
                         )}
                     )
                 } 
-            </div>*/}
+
         </div>
     </>
-            
-  
     )
 }
